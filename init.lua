33140af2c43fedd7fd559b3f6ae480509fb380b2
Rafal <rafaraf@o2.pl>
2017-04-12 10:50:28 +0200

    Change in the main page and init script

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 --This file is init.lua
-local IDLE_AT_STARTUP_MS = 1500;
+local IDLE_AT_STARTUP_MS = 4000;
 
 tmr.alarm(1,IDLE_AT_STARTUP_MS,0,function()
     dofile("application.lua") --Write your program name in dofile
